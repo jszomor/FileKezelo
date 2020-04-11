fb215642daa4862f5220e6c975d5f57ef9ed2059
Szomor János <jszomor@gmail.com>
2020-04-11 12:57:36 +0200

    Add project files.

---
+++ FileKezelo/MyFile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileKezelo
+{
+  public class MyFile
+  {
+    StreamReader FileReader;
+    StreamWriter FileWriter;
+    FileStream StreamFile;
+
+    public MyFile(string fileName)
+    {
+      try
+      {
+        StreamFile = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite);
+        FileReader = new StreamReader(StreamFile);
+        FileWriter = new StreamWriter(StreamFile);
+      }
+      catch(IOException e)
+      {
+        string customMessage = e.Message;
+        int errorCode = e.HResult;
+        ErrorHandler.PutError(customMessage, errorCode);
+        throw new IOException($"message:{customMessage}, \n errorcode:{errorCode}");
+      }
+    }
+  }
+  public static class ErrorHandler
+  {
+    public static int ErrorCode;
+    public static int RiskLevel;
+    public static int FieldOfError;
+    public static string CustomMessage;
+
+    public static void PutError(string customMessage, int errorCode)
+    {
+      //értéket ír a 3 property be
+      ErrorCode = errorCode;
+      CustomMessage = customMessage;
+      Console.WriteLine("ErrorCode: {0}",ErrorCode);
+      Console.WriteLine("Message: {0}", CustomMessage);
+      Console.ReadKey();
+    }
+    public static void GetError()
+    {
+      //kiveszti az értéket a 3 property ből
+    }
+  }
+}
---
+++ FileKezelo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileKezelo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string fileName = "text.txt"; 
+      try
+      {
+        var myFile = new MyFile(fileName);
+      }
+      catch(IOException ioEx)
+      {
+        Console.WriteLine($"bocs valami rosszul ment: \n {ioEx}");
+      }
+      finally
+      {
+        Console.WriteLine("Program vége");
+      }
+      Console.ReadKey();   
+    }
+  }
+}
